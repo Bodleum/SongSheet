1da3a11180cd166befb67478c5b850e39700a9f8
Bodleum <git@daniellaing.com>
2021-12-06 22:10:07 +0000

    Now automatically generating `.tex` files

--- compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sed -i 's/musicmode{[0-9]/musicmode{1/' SongSheet.tex
-latexmk -pdflua SongSheet.tex
-mv SongSheet.pdf SongSheetWithChords.pdf
-sed -i 's/musicmode{[0-9]/musicmode{0/' SongSheet.tex
-latexmk -pdflua SongSheet.tex
-latexmk -c
---
+++ make.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Increment version number
+ver=$(grep -Po \\d\.\\d\.\\d\\d Head.tex | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}')
+sed -i "s/ssver [0-9]\.[0-9]\.[0-9][0-9]/ssver $ver/" Head.tex
+
+# Build
+cat Head.tex Songs/*.tex Psalms/* Foot.tex > SongSheet.tex
+
+# Compile
+sed -i 's/musicmode{[0-9]/musicmode{1/' SongSheet.tex
+latexmk -pdflua SongSheet.tex
+mv SongSheet.pdf SongSheetWithChords.pdf
+sed -i 's/musicmode{[0-9]/musicmode{0/' SongSheet.tex
+latexmk -pdflua SongSheet.tex
+latexmk -c
